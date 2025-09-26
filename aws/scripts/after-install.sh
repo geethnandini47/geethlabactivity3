a0afb8161e881058e58ab718cb49f637f20f65c2
geethnandini47 <geethnandinichukkareddy@gmail.com>
2025-09-25 21:59:13 -0400

    commit

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -xe
 
+# Variables
+S3_BUCKET="codedeploystack-webappdeploymentbucket-lymvz0np9mdj"
+WAR_FILE="SpringBootHelloWorldExampleApplication.war"
+TOMCAT_WEBAPPS="/usr/local/tomcat9/webapps"
+
 # Copy WAR file from S3 bucket to Tomcat webapps folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-lymvz0np9mdj/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://$S3_BUCKET/$WAR_FILE $TOMCAT_WEBAPPS/$WAR_FILE
 
 # Ensure the ownership and permissions are correct
-chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+chown -R tomcat:tomcat $TOMCAT_WEBAPPS
