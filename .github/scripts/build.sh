76cdbebd8e0e65f865e446c0126d9da6bab606fb
geethnandini47 <geethnandinichukkareddy@gmail.com>
2025-09-25 20:20:42 -0400

    commit new changes

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -xe
 
-  # Maven is used to build  and create a war file.
-  mvn -Dmaven.test.skip=true clean install
+echo "=== Running AfterInstall script ==="
 
+# Go to home directory
+cd /home/ec2-user
 
+# Download the latest WAR file from your S3 Deployment Bucket
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-lymvz0np9mjd/spring-boot-hello-world-example.war /home/ec2-user/app.war
+
+# Ensure proper permissions
+chmod 755 /home/ec2-user/app.war
+
+echo "=== AfterInstall script completed successfully ==="
