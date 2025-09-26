77971be2ed8068b2d805f8f531f93d9a1ed6d5fb
geethnandini47 <geethnandinichukkareddy@gmail.com>
2025-09-25 20:30:41 -0400

    corrected

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,6 +1,12 @@
-
 #!/bin/bash
 set -xe
 
-  # Maven is used to build  and create a war file.
-  mvn -Dmaven.test.skip=true clean install
+echo "=== Running Build Script on GitHub Actions ==="
+
+# Navigate into the app folder
+cd $GITHUB_WORKSPACE/spring-boot-hello-world-example
+
+# Build the WAR file using Maven
+mvn -Dmaven.test.skip=true clean install
+
+echo "=== Build completed. WAR file is in target/ directory ==="
